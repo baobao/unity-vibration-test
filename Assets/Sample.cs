e615822d3cc1940dad43905143eef6b62144fabc
Shunsuke Ohba <shunsuke.ohba@gmail.com>
2017-10-01 23:57:55 +0900

    Add Vibration Plugins

---
+++ Assets/Sample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using info.shibuya24;
+
+public class Sample : MonoBehaviour 
+{
+    void OnGUI ()
+    {
+        if (GUILayout.Button ("Vibration 1", GUILayout.Width (300f), GUILayout.Height (200f)))
+        {
+            VibrationPlugin.PlayVibration (1);
+        }
+        if (GUILayout.Button ("Vibration 2", GUILayout.Width (300f), GUILayout.Height (200f)))
+        {
+            VibrationPlugin.PlayVibration (2);
+        }
+        if (GUILayout.Button ("Vibration 5", GUILayout.Width (300f), GUILayout.Height (200f)))
+        {
+            VibrationPlugin.PlayVibration (5);
+        }
+        if (GUILayout.Button ("Vibration Unity", GUILayout.Width (300f), GUILayout.Height (200f)))
+        {
+            Handheld.Vibrate ();
+        }
+    }
+}
